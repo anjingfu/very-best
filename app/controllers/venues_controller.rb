66297834254c8831e592ad0bc5b3baef3a25dc22
Arthur Jacobs <ajacobs15@uchicago.edu>
2018-03-09 23:48:45 +0000

    Completed Venues Tests
    its lit

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -5,8 +5,8 @@ class VenuesController < ApplicationController
     #Venue.where(:id in bookmarks{:venue_id})
     #venues = bookmarks.uniq.pluck(:venue_id)
     
-    @q = Venue.ransack(params[:q])
-    @venues = @q.result(:distinct => true).includes(:bookmarks).page(params[:page]).per(10)
+    @q = Venue.where("venues.bookmarks_count > 0").ransack(params[:q])
+    @venues = @q.result(:distinct => true).includes(:bookmarks, :neighborhood, :fans, :specialties).page(params[:page]).per(10)
     @location_hash = Gmaps4rails.build_markers(@venues.where.not(:address_latitude => nil)) do |venue, marker|
       marker.lat venue.address_latitude
       marker.lng venue.address_longitude
