e5b0342625e8cc5501a80a3db5657fa85b37d78b
Arthur Jacobs <ajacobs15@uchicago.edu>
2018-03-11 01:42:07 +0000

    done with venues show

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -19,7 +19,7 @@ class Bookmark < ApplicationRecord
   validates :user_id, :presence => true
 
   validates :venue_id, :uniqueness => { :scope => [:dish_id, :user_id], :message => "has already been bookmarked" }
-
+  
   validates :venue_id, :presence => true
 
 end
